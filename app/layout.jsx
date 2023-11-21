6109e2970cfb67db1225f341b21cace65e6f281d
Tahiel Pedevilla <tahielpeed@gmail.com>
2023-11-21 20:18:39 -0300

    fix metadata final

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,17 +10,6 @@ export const metadata = {
 	title: "Tahiel Pedevilla",
 	description:
 		"Tahiel Pedevilla is a Web Developer specializing in Frontend Development and Webflow Development.",
-	openGraph: {
-		title: "Tahiel Pedevilla",
-		description: "Tahiel Pedevilla is a Web Developer specializing in Frontend Development and Webflow Development.",
-		image: "https://www.tahielpedevilla.com/og.png",
-	},
-	twitter: {
-		card: "summary_large_image",
-		title: "Tahiel Pedevilla",
-		description: "Tahiel Pedevilla is a Web Developer specializing in Frontend Development and Webflow Development.",
-		image: "https://www.tahielpedevilla.com/og.png",
-	},
 }
 
 export default function RootLayout({ children }) {
