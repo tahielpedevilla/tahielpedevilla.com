c3b60b9eaa3777dcdbba014b44c1b3cec68b8458
tahielpedevilla <tahielpeed@gmail.com>
2024-07-14 21:10:03 -0300

    update hero padding

--- app/page.jsx
+++ app/page.jsx
@@ -53,7 +53,7 @@ export default function Page() {
 		<main>
 			<section
 				id="home"
-				className="flex flex-col justify-center items-center py-[25vh] relative"
+				className="flex flex-col justify-center items-center py-[20svh] md:py-[25svh] relative"
 			>
 				<div className="flex flex-col justify-center items-center mx-auto container px-6 z-10">
 					<h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl tracking-tight leading-tight text-center">
